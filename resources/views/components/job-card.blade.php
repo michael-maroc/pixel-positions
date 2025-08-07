040fedd50f5b96bb62e9cfa3080e91b024956e32
michael <maroc.michael@hotmail.com>
2025-08-07 20:30:19 +0200

    add job-card component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracats</div>
+
+    <div class="py-8 font-bold">
+        <div>Video Producer</div>
+        <div>Full Time - From $60,000</div>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+        </div>
+    </div>
+
+    <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+</div>
